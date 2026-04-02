6f4c07e099350683a756d2dc542013dca55fd6fc
EmanuelCandido <emanuelcandidolima@gmail.com>
2026-04-02 18:49:15 -0300

    Inicio do projeto criando main e a classe ingresso com valor e metodo imprimeValor.

---
+++ src/main/java/aula/questao_3/Ingresso.java
@@ -0,0 +1,11 @@
+
+package aula.questao_3;
+
+
+public class Ingresso {
+    private double valor;
+    
+    public void imprimeValor(){
+        System.out.println(this.valor);
+    }
+}
---
+++ src/main/java/aula/questao_3/Questao_3.java
@@ -0,0 +1,11 @@
+
+
+package aula.questao_3;
+
+
+public class Questao_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
