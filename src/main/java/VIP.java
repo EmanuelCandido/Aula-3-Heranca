1fb60a0cd395d54c8b73a611b569bfae0d702276
EmanuelCandido <emanuelcandidolima@gmail.com>
2026-04-02 19:02:05 -0300

    Criei o metodo set no ingresso e criei a classe vip com valor adicional

---
+++ src/main/java/VIP.java
@@ -0,0 +1,14 @@
+
+
+package aula.questao_3;
+
+
+public class VIP extends Ingresso {
+    
+    @Override
+    public void setValor(double valor){
+        super.setValor(valor+5);
+    }
+    
+    
+}
--- src/main/java/aula/questao_3/Ingresso.java
+++ src/main/java/aula/questao_3/Ingresso.java
@@ -4,8 +4,14 @@ package aula.questao_3;
 
 public class Ingresso {
     private double valor;
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
     
-    public void imprimeValor(){
-        System.out.println(this.valor);
+    
+    
+    public double imprimeValor(){
+        return this.valor;
     }
 }
