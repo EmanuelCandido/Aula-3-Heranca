1190bdd2ceb130e669d164eea9ede4d13575e25f
EmanuelCandido <emanuelcandidolima@gmail.com>
2026-04-02 19:24:24 -0300

    Classe normal completa

--- src/main/java/aula/questao_3/Normal.java
+++ src/main/java/aula/questao_3/Normal.java
@@ -2,6 +2,15 @@
 package aula.questao_3;
 
 public class Normal extends Ingresso {
+
+    public Normal(double valor) {
+        super(valor);
+    }
     
-    
+    @Override
+    public double imprimeValor(){
+        System.out.println("Valor do ingresso Normal; "+ getValor());
+        return super.imprimeValor();
+        
+    }
 }
--- src/main/java/aula/questao_3/Questao_3.java
+++ src/main/java/aula/questao_3/Questao_3.java
@@ -7,9 +7,9 @@ public class Questao_3 {
 
     public static void main(String[] args) {
         VIP quero = new VIP(15);
-        
+        Normal sera = new Normal(15);
        double valor = quero.imprimeValor();
-       
+       double valia = sera.imprimeValor();
        
     }
 }
